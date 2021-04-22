2d0225e31c9a3cf2975addac026a6dcd7004d7a7
boutsism <minasmpoutsis@gmail.com>
2021-04-22 12:32:32 +0300

    adding greek language
    Planning to add the greek language as second

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var target = document.querySelector(".section4");
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
-var rootElement = document.documentElement;
+const target = document.querySelector(".work");
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+const rootElement = document.documentElement;
 
 function callback(entries, observer) {
   entries.forEach(entry => {
@@ -24,53 +24,21 @@ scrollToTopBtn.addEventListener("click", scrollToTop);
 let observer = new IntersectionObserver(callback);
 observer.observe(target);
 
-function about() {
-  const heading2 = document.querySelector("h2")
-  heading2.scrollIntoView({
-    behavior: 'smooth', // smooth scroll
-    block: 'center' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
-  })
-}
-
-function projects() {
-  const heading3 = document.querySelector(".section3")
-  heading3.scrollIntoView({
-    behavior: 'smooth', // smooth scroll
-    block: 'center' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
-  })
-}
-
-function contact() {
-  const heading4 = document.querySelector("h4")
-  heading4.scrollIntoView({
-    behavior: 'smooth', // smooth scroll
-    block: 'center' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
-  })
-}
-
-document.addEventListener("DOMContentLoaded", function() { 
-  const myNav = document.querySelector('.navbar-light');
-  window.onscroll = function () {
-    if (document.documentElement.scrollTop > 1000) {
-      myNav.classList.add("nav-scrolled");
-      myNav.classList.remove("nav-scrolled1");
-    }
-    else if (document.documentElement.scrollTop > 500) {
-      myNav.classList.add("nav-scrolled1");
-      myNav.classList.remove("nav-scrolled");
-
-    }
-    else if (document.documentElement.scrollTop > 50) {
-      myNav.classList.add("nav-scrolled");
-      myNav.classList.remove("nav-scrolled1");
-    }
-    else {
-      myNav.classList.remove("nav-scrolled1");
-      myNav.classList.remove("nav-scrolled");
-    }
-    AOS.init();
-  }
+//Navbar scroll to spots
+const choices = document.querySelectorAll(".nav-choices")
+const spot = ["h2", "h3", "h4"];
+choices.forEach(function (item, index) {
+  const elmnt = document.querySelector(spot[index]);
+  item.addEventListener('click', function () {
+    elmnt.scrollIntoView({
+      block: "start"
+    });
+  });
 });
+
+window.onscroll = function () {
+  AOS.init();
+}
 
 /*const cookieBox = document.querySelector(".cookiebar"),
   acceptBtn = cookieBox.querySelector(".btn");
