ce27d4977d4fd743db79f7ca4c0b3f4af1fdaf43
boutsism <76609640+boutsism@users.noreply.github.com>
2023-11-05 18:41:41 +0200

    Add files via upload
    made buttons working

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const mobileBar = document.querySelector('nav .hidden');
 navButton.addEventListener('click', () => {
 	mobileBar.classList.toggle('hidden');
 });
+function scrollAbout() {
+	const learnMore = document.querySelector('.about');
+	learnMore.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
+function scrollProjects() {
+	const Projects = document.querySelector('.projects');
+	Projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+function scrollContact() {
+	const Contact = document.querySelector('.contact');
+	Contact.scrollIntoView({ behavior: 'smooth', block: 'end' });
+}
+function scrollToTop() {
+    scrollTo({ top: 0, behavior: "smooth" });
+  }
